83bac843216805e4e5d8cead9d9de5cc3654eba8
jgately707 <gatelyj231@gmail.com>
2025-05-11 18:27:10 +0700

    accepted all changes from teammates in session.ts

---
+++ src/client/socket/index.ts
@@ -0,0 +1,26 @@
+import io from "socket.io-client";
+
+const socket = io();
+
+export { socket };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+Documentation:
+- install npm i socket.io-client and  npm i @types/socket.io-client
+
+*/
